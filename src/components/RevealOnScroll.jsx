db2801bb0ecba8903f2c19d7fa75a579da5c4096
mukundakrishnaramisetti <mukundakrishna.r@gmail.com>
2026-03-30 20:43:22 -0500

    scroll fix

--- src/components/RevealOnScroll.jsx
+++ src/components/RevealOnScroll.jsx
@@ -10,13 +10,16 @@ export const RevealOnScroll = ({ children }) => {
           ref.current.classList.add("visible");
         }
       },
-      { threshold: 0.2, rootMargin: "0px 0px -50px 0px" }
+      {
+        threshold: 0.01,       // Trigger when just 1% is visible (fixes mobile tall sections)
+        rootMargin: "0px 0px 50px 0px", // Start revealing 50px BEFORE entering viewport
+      }
     );
 
     if (ref.current) observer.observe(ref.current);
 
     return () => observer.disconnect();
-  });
+  }, []);
 
   return (
     <div ref={ref} className="reveal">
